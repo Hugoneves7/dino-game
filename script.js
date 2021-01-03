39ef8dd026fa1669ba3be18f4e8335499749b3b5
Hugo Neves <hugo.neves@outlook.com>
2021-01-03 10:48:39 -0300

    v1

---
+++ script.js
@@ -0,0 +1,72 @@
+const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let isjumping = false;
+let position = 0; //calcula o pulo
+
+
+function handleKeyUp(event) {
+  if (event.keyCode === 32) {
+    if (!isjumping) {
+      jump();
+    }
+  }
+}
+function jump() {
+  isjumping = true;
+
+  let upInterval = setInterval(() => {
+    if (position >= 150) {
+      clearInterval(upInterval);
+
+      //descendo
+      let downInterval = setInterval(() => {
+        if (position <= 0) {
+          clearInterval(downInterval);
+          isjumping = false;
+        } else {
+          position -= 20;
+          dino.style.bottom = position + 'px';
+        }
+      }, 20);
+    } else {
+      //subindo
+      position += 20;
+      dino.style.bottom = position + 'px';
+    }
+  }, 20);
+}
+
+function createCactus() {
+  const cactus = document.createElement('div');
+  let cactusPosition = 1000;
+  //Math op matematicas, arredondamento.
+  //random numero aleatorio.
+  let randomTime = Math.random() * 6000;
+
+  cactus.classList.add('cactus');
+  cactus.style.left = 1000 + 'px';
+  background.appendChild(cactus);
+
+  let leftInterval = setInterval(() => {
+    //pro cactus desaparecer/remover quando sair da tela.
+    if (cactusPosition < -60) {
+      clearInterval(leftInterval);
+      background.removeChild(cactus);
+      //continua se movimentando
+      //verifica se o dino bateu no cactus
+    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+      //game over
+      clearInterval(leftInterval);
+      document.body.innerHTML = '<h1 class="game-over">Fim do jogo!</h1>';
+    } else {
+      //posição do cactus/velocidade
+      cactusPosition -= 10;
+      cactus.style.left = cactusPosition + 'px';
+    }
+  }, 20);
+  //executa função depois de um determinado tempo.
+  //invocar cactos, recursividade
+  setTimeout(createCactus, randomTime);
+}
+createCactus();
+document.addEventListener('keyup', handleKeyUp);
